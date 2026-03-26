7ae263e0b5fedb96866818c4082d352d2b30d45c
luish-uk <luishernandezlg77@gmail.com>
2026-03-26 10:22:31 +0000

    Printed SQL fetchall on flask server

--- app.py
+++ app.py
@@ -1,37 +1,39 @@
 from flask import Flask, render_template
 import serial 
-
+from database import init_db, insert_reading, get_readings
+import json
 
 app = Flask(__name__)
+
 ser = serial.Serial('/dev/ttyACM0', 9600)
+
+init_db()
 
 data = ser.readline().decode().strip()
 
 @app.route("/")
 def home():
-    data = ser.readline().decode().strip()
-    percent = data
-    percent = percent["light"]
-    return render_template('index.html', brightness = percent)
+    data_dict = json.loads(data)
+    light_percent = data_dict["light"]
+
+    insert_reading(light_percent)
+    readings = get_readings()
+    return render_template('index.html', readings=readings)
 
 app.run(host="0.0.0.0", port=5000)
-class Data:
-    def __init__(self):
-        self.lightData = data
-        return self.data
-    
-    def insert(self, column):
-        line = "INSERT INTO {column} VALUES({self.lightData})"
-        return line
-    def query(self, column=None, table=None):
-        if table == None:
-            table = "sqlite_master"
-        if column == None:
-            column = "*"
-        else:
-            columns = ""
-            l = len(column)
-            for i in range(l-2):
-                columns += f'{column[i]}, '
-            columns += f'{column[-1]}'
-        line = "SELECT {column}"
+# class Data:
+#     def insert(self, column):
+#         line = "INSERT INTO {column} VALUES({self.lightData})"
+#         return line
+#     def query(self, column=None, table=None):
+#         if table == None:
+#             table = "sqlite_master"
+#         if column == None:
+#             column = "*"
+#         else:
+#             columns = ""
+#             l = len(column)
+#             for i in range(l-2):
+#                 columns += f'{column[i]}, '
+#             columns += f'{column[-1]}'
+#         line = "SELECT {column}"
--- database.py
+++ database.py
@@ -1,6 +1,26 @@
 import sqlite3
-con = sqlite3.connect("Database.db")
 
-cur = con.cursor()
 
-cur.execute("CREATE TABLE Light_Sensor(percent int)")
+
+
+def init_db():
+    connect = sqlite3.connect("Sensor.db")
+    cursor = connect.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS readings(id INTEGER PRIMARY KEY AUTOINCREMENT, light_percent INT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);""")
+    connect.commit()
+    connect.close()
+
+def insert_reading(light_percent):
+    connect = sqlite3.connect("Sensor.db")
+    cursor = connect.cursor()
+    cursor.execute(f"INSERT INTO readings (light_percent) VALUES ({light_percent});")
+    connect.commit()
+    connect.close()
+
+def get_readings():
+    connect = sqlite3.connect("Sensor.db")
+    cursor = connect.cursor()
+    cursor.execute(f"SELECT * FROM readings;")
+    results = cursor.fetchall()
+    connect.close()
+    return results
